a593b0cc9b4f86179bdbdf18a69a972c419e50aa
Tony Gyan <49129216+TONYOG12@users.noreply.github.com>
2022-10-14 21:05:06 +0000

    Update 81. search in sorted array II.py

--- questions/81. search in sorted array II.py
+++ questions/81. search in sorted array II.py
@@ -1,11 +1,18 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
         
+        #return true if index of target value exists in array
         try:
+            #python sees 0 as false so need to check for that index
             if nums.index(target) == 0:
                 return True
-            
             return nums.index(target)
         except:
             return False
+        
+        
+        #Time Complexity = O(n)
+        #Space Complexity = 0(1)
+        
+        #Utilize binary search for better time complexity
         
