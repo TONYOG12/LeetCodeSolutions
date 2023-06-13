155cbee365b72ea45a081ac2bf2afc2e1eeb8d66
Tony Gyan <49129216+TONYOG12@users.noreply.github.com>
2023-06-13 12:44:32 +0000

    Create 109. Convert Sorted List To BST.py

---
+++ questions/109. Convert Sorted List To BST.py
@@ -0,0 +1,49 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
+
+
+        #def divideList(head):
+
+        if not head:
+            return None
+
+        if not head.next:
+            return TreeNode(head.val)
+
+
+        slow, fast = head, head.next.next
+
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+
+        mid = slow.next
+
+
+        leftHead = head
+        rightHead = slow.next.next
+
+
+        slow.next = None
+
+
+
+        root = TreeNode(mid.val)
+        root.left = self.sortedListToBST(leftHead)
+        root.right = self.sortedListToBST(rightHead)
+
+
+        return root
